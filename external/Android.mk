7c3d3bd7316a02761a7ae07b6ab14d4f10fee6d2
Hans-Christoph Steiner <hans@eds.org>
2011-02-22 20:04:09 -0500

    libsqlcipher.so builds

--- external/Android.mk
+++ external/Android.mk
@@ -18,8 +18,33 @@ sqlcipher/sqlite3.c:
 
 
 #------------------------------------------------------------------------------#
-# libsqlite3_android
+# libsqlite3
 
+# NOTE the following flags,
+#   SQLITE_TEMP_STORE=3 causes all TEMP files to go into RAM. and thats the behavior we want
+#   SQLITE_ENABLE_FTS3   enables usage of FTS3 - NOT FTS1 or 2.
+#   SQLITE_DEFAULT_AUTOVACUUM=1  causes the databases to be subject to auto-vacuum
+android_sqlite_cflags :=  -DHAVE_USLEEP=1 -DSQLITE_DEFAULT_JOURNAL_SIZE_LIMIT=1048576 -DSQLITE_THREADSAFE=1 -DNDEBUG=1 -DSQLITE_ENABLE_MEMORY_MANAGEMENT=1 -DSQLITE_DEFAULT_AUTOVACUUM=1 -DSQLITE_TEMP_STORE=3 -DSQLITE_ENABLE_FTS3 -DSQLITE_ENABLE_FTS3_BACKWARDS
+
+sqlcipher_files := \
+	sqlcipher/sqlite3.c
+
+sqlcipher_cflags := -DSQLITE_HAS_CODEC -DHAVE_FDATASYNC=0 -Dfdatasync=fsync
+sqlcipher_ldflags:= -Lopenssl/libs/armeabi -lcrypto
+
+include $(CLEAR_VARS)
+
+LOCAL_CFLAGS += $(android_sqlite_cflags) $(sqlcipher_cflags)
+LOCAL_C_INCLUDES := openssl/include sqlcipher
+LOCAL_LDFLAGS += $(sqlcipher_ldflags)
+LOCAL_MODULE    := libsqlcipher
+LOCAL_SRC_FILES := $(sqlcipher_files)
+
+include $(BUILD_SHARED_LIBRARY)
+
+
+#------------------------------------------------------------------------------#
+# libsqlite3_android
 
 # these are all files from various external git repos
 libsqlite3_android_local_src_files := \
@@ -43,29 +68,3 @@ include $(BUILD_SHARED_LIBRARY)
 ## this might save us linking against the private android shared libraries like
 ## libnativehelper.so, libutils.so, libcutils.so, libicuuc, libicui18n.so
 # LOCAL_ALLOW_UNDEFINED_SYMBOLS := true
-
-
-#------------------------------------------------------------------------------#
-# libsqlite3
-
-# NOTE the following flags,
-#   SQLITE_TEMP_STORE=3 causes all TEMP files to go into RAM. and thats the behavior we want
-#   SQLITE_ENABLE_FTS3   enables usage of FTS3 - NOT FTS1 or 2.
-#   SQLITE_DEFAULT_AUTOVACUUM=1  causes the databases to be subject to auto-vacuum
-android_sqlite_cflags :=  -DHAVE_USLEEP=1 -DSQLITE_DEFAULT_JOURNAL_SIZE_LIMIT=1048576 -DSQLITE_THREADSAFE=1 -DNDEBUG=1 -DSQLITE_ENABLE_MEMORY_MANAGEMENT=1 -DSQLITE_DEFAULT_AUTOVACUUM=1 -DSQLITE_TEMP_STORE=3 -DSQLITE_ENABLE_FTS3 -DSQLITE_ENABLE_FTS3_BACKWARDS
-
-sqlcipher_files := \
-	sqlcipher/sqlite3.c
-
-sqlcipher_cflags := -DSQLITE_HAS_CODEC -DHAVE_FDATASYNC=0 -Dfdatasync=fsync
-sqlcipher_ldflags:= -L$(EXTERNAL_ROOT)/openssl/libs/armeabi -lcrypto
-
-include $(CLEAR_VARS)
-
-LOCAL_CFLAGS += $(android_sqlite_cflags) $(sqlcipher_cflags)
-LOCAL_C_INCLUDES := openssl/include sqlcipher
-LOCAL_LDFLAGS += $(sqlcipher_ldflags)
-LOCAL_MODULE    := libsqlcipher
-LOCAL_SRC_FILES := $(sqlcipher_files)
-
-include $(BUILD_SHARED_LIBRARY)
