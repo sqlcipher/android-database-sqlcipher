d679a3bce749fa8b3ffa685bc74063f1760260d1
Nathan Freitas <nathan@freitas.net>
2011-04-29 15:25:56 -0700

    adding in the compiled so files into the project

---
+++ libs/armeabi/objs/database_sqlcipher/info_guardianproject_database_sqlcipher_SQLiteDebug.o.d
@@ -0,0 +1,206 @@
+/home/hans/code/guardianproject/android-database-sqlcipher/obj/local/armeabi/objs/database_sqlcipher/info_guardianproject_database_sqlcipher_SQLiteDebug.o: \
+ /home/hans/code/guardianproject/android-database-sqlcipher/jni/info_guardianproject_database_sqlcipher_SQLiteDebug.cpp \
+ ../external/dalvik/libnativehelper/include/nativehelper/JNIHelp.h \
+ ../external/dalvik/libnativehelper/include/nativehelper/jni.h \
+ /usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stdarg.h \
+ /usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/_cprolog.h \
+ /usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/features.h \
+ /usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h \
+ /usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/user_config.h \
+ /usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/compat.h \
+ /usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/host.h \
+ /usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_system.h \
+ /usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_android.h \
+ /usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h \
+ /usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h \
+ /usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h \
+ /usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/_epilog.h \
+ /usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h \
+ /home/hans/code/guardianproject/android-database-sqlcipher/jni/include/utils/Log.h \
+ ../external/platform-system-core/include/android/log.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/unistd.h \
+ /usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stddef.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/sys/select.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ /usr/local/android-ndk/sources/cxx-stl/stlport/stlport/signal.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/../include/signal.h \
+ /usr/local/android-ndk/sources/cxx-stl/stlport/stlport/limits.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/../include/limits.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /usr/local/android-ndk/sources/cxx-stl/stlport/stlport/string.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/../include/string.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/asm/signal.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/sys/sysconf.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/linux/capability.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/pathconf.h \
+ ../external/dalvik/libnativehelper/include/nativehelper/jni.h \
+ ../external/platform-frameworks-base/include/utils/misc.h \
+ ../external/platform-frameworks-base/include/utils/Endian.h \
+ /usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stdio.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/../include/stdio.h \
+ /usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stdlib.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/../include/stdlib.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/strings.h \
+ /usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/memory.h \
+ ../external/platform-system-core/include/cutils/mspace.h \
+ ../external/sqlcipher/sqlite3.h
+
+../external/dalvik/libnativehelper/include/nativehelper/JNIHelp.h:
+
+../external/dalvik/libnativehelper/include/nativehelper/jni.h:
+
+/usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stdarg.h:
+
+/usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/_cprolog.h:
+
+/usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/features.h:
+
+/usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h:
+
+/usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/user_config.h:
+
+/usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/compat.h:
+
+/usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/host.h:
+
+/usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_system.h:
+
+/usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_android.h:
+
+/usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h:
+
+/usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h:
+
+/usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h:
+
+/usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/_epilog.h:
+
+/usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h:
+
+/home/hans/code/guardianproject/android-database-sqlcipher/jni/include/utils/Log.h:
+
+../external/platform-system-core/include/android/log.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/unistd.h:
+
+/usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stddef.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/sys/select.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+/usr/local/android-ndk/sources/cxx-stl/stlport/stlport/signal.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/../include/signal.h:
+
+/usr/local/android-ndk/sources/cxx-stl/stlport/stlport/limits.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/../include/limits.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/usr/local/android-ndk/sources/cxx-stl/stlport/stlport/string.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/../include/string.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/asm/signal.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/sys/sysconf.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/linux/capability.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/pathconf.h:
+
+../external/dalvik/libnativehelper/include/nativehelper/jni.h:
+
+../external/platform-frameworks-base/include/utils/misc.h:
+
+../external/platform-frameworks-base/include/utils/Endian.h:
+
+/usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stdio.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/../include/stdio.h:
+
+/usr/local/android-ndk/sources/cxx-stl/stlport/stlport/stdlib.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/../include/stdlib.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/usr/local/android-ndk/platforms/android-3/arch-arm/usr/include/memory.h:
+
+../external/platform-system-core/include/cutils/mspace.h:
+
+../external/sqlcipher/sqlite3.h:
