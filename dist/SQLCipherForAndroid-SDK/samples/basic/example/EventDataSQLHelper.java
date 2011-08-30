e05c6693800ade437b4cca00c19904708b7f1a6c
Nathan Freitas <nathan@freitas.net>
2011-08-30 16:52:34 -0400

    adding in SDK distribution into the repo for tracking/versioning

---
+++ dist/SQLCipherForAndroid-SDK/samples/basic/example/EventDataSQLHelper.java
@@ -0,0 +1,50 @@
+package example;
+
+import info.guardianproject.database.sqlcipher.SQLiteDatabase;
+import info.guardianproject.database.sqlcipher.SQLiteOpenHelper;
+import android.content.Context;
+import android.provider.BaseColumns;
+import android.util.Log;
+
+/** Helper to the database, manages versions and creation */
+public class EventDataSQLHelper extends SQLiteOpenHelper {
+	private static final String DATABASE_NAME = "events.db";
+	private static final int DATABASE_VERSION = 1;
+
+	// Table name
+	public static final String TABLE = "events";
+
+	// Columns
+	public static final String TIME = "time";
+	public static final String TITLE = "title";
+
+	public EventDataSQLHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		String sql = "create table " + TABLE + "( " + BaseColumns._ID
+				+ " integer primary key autoincrement, " + TIME + " integer, "
+				+ TITLE + " text not null);";
+		Log.d("EventsData", "onCreate: " + sql);
+		db.execSQL(sql);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		if (oldVersion >= newVersion)
+			return;
+
+		String sql = null;
+		if (oldVersion == 1) 
+			sql = "alter table " + TABLE + " add note text;";
+		if (oldVersion == 2)
+			sql = "";
+
+		Log.d("EventsData", "onUpgrade	: " + sql);
+		if (sql != null)
+			db.execSQL(sql);
+	}
+
+}
