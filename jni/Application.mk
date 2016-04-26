a3cf5c5517e69b2ee4f5f414f55fc76c80563fae
Nick Parker <nparker@zetetic.net>
2016-04-26 16:02:18 -0500

    Clear repo content

--- jni/Application.mk
+++
@@ -1,6 +0,0 @@
-APP_PROJECT_PATH := $(shell pwd)
-APP_ABI := armeabi armeabi-v7a x86
-APP_BUILD_SCRIPT := $(APP_PROJECT_PATH)/Android.mk
-# fixes this error when building external/android-sqlite/android/sqlite3_android.cpp
-#   icu4c/common/unicode/std_string.h:39:18: error: string: No such file or directory
-APP_STL := stlport_shared
--- src/net/sqlcipher/database/SQLiteDatabaseHook.java
+++
@@ -1,6 +0,0 @@
-package net.sqlcipher.database;
-
-public interface SQLiteDatabaseHook {
-    void preKey(SQLiteDatabase database);
-    void postKey(SQLiteDatabase database);
-}
