0c4fc46da8dc5ca16f09f8e755fdf2dc584d07ea
Nick Parker <nparker@zetetic.net>
2016-05-23 10:33:43 -0500

    Prevent logging when LOG_NDEBUG is specified

--- jni/log.h
+++ jni/log.h
@@ -1,9 +1,16 @@
 #include <android/log.h>
 
+#ifdef LOG_NDEBUG
+#define LOGI(...)
+#define LOGE(...)
+#define LOGV(...)
+#define LOGD(...)
+#else
 #define LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 #define LOGV(...)  __android_log_print(ANDROID_LOG_VERBOSE,LOG_TAG,__VA_ARGS__)
 #define LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
+#endif
 
 #ifndef LOG
 #define LOG(priority, tag, ...) \
