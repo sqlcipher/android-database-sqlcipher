1053ca77ec383ac39dcd861955c9955977570fb3
Nick Parker <nparker@zetetic.net>
2016-05-23 12:05:03 -0500

    Set Android target platform to 21 for NDK

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,5 @@
 APP_PROJECT_PATH := $(shell pwd)
 APP_ABI := armeabi armeabi-v7a x86
-# APP_ABI := armeabi
+APP_PLATFORM := 21
 APP_BUILD_SCRIPT := $(APP_PROJECT_PATH)/Android.mk
 APP_STL := c++_static
