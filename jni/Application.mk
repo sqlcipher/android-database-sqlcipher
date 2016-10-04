d0981aa7ee5b232dbcf7a2155d48e7e66952824f
Alex Trotsenko <olexii.trotsenko@gmail.com>
2016-10-04 18:07:07 +0200

    #264 update APP_PLATFORM to 23: fixes crash on Android 6 with build with latest android-ndk-r12b

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,8 @@
+# If set to 24 then app is crashing on Android 6 with "java.lang.UnsatisfiedLinkError: dlopen failed: cannot locate symbol "__aeabi_memclr4" referenced by "/data/app/com.spotme.android-1/lib/arm/libsqlcipher.so"""
+#See https://github.com/android-ndk/ndk/issues/126
+APP_PLATFORM=android-23
+
 APP_PROJECT_PATH := $(shell pwd)
 APP_ABI := armeabi armeabi-v7a x86
-APP_PLATFORM := 21
 APP_BUILD_SCRIPT := $(APP_PROJECT_PATH)/Android.mk
 APP_STL := stlport_static
