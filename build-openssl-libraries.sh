20982589b957597b57e137f0ea4441c953218fc6
Nick Parker <nparker@zetetic.net>
2017-01-11 14:52:45 -0600

    Build 64-bit support for libcrypto.a (x86_64 and arm64-v8a)

--- build-openssl-libraries.sh
+++ build-openssl-libraries.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 (cd external/openssl;
 
     if [ ! ${ANDROID_NDK_ROOT} ]; then
@@ -7,6 +7,16 @@
     fi
 
     ANDROID_LIB_ROOT=../android-libs
+    ANDROID_PLATFORM_VERSION=android-23
+    ANDROID_TOOLCHAIN_DIR=/tmp/sqlcipher-android-toolchain
+    OPENSSL_CONFIGURE_OPTIONS="no-krb5 no-idea no-camellia \
+        no-seed no-bf no-cast no-rc2 no-rc4 no-rc5 no-md2 \
+        no-md4 no-ripemd no-rsa no-ecdh no-sock no-ssl2 no-ssl3 \
+        no-dsa no-dh no-ec no-ecdsa no-tls1 no-pbe no-pkcs \
+        no-tlsext no-pem no-rfc3779 no-whirlpool no-ui no-srp \
+        no-ssltrace no-tlsext no-mdc2 no-ecdh no-engine \
+        no-tls2 no-srtp -fPIC"
+    
     HOST_INFO=`uname -a`
     case ${HOST_INFO} in
         Darwin*)
@@ -27,75 +37,76 @@
     esac
 
     rm -rf ${ANDROID_LIB_ROOT}
-    mkdir -p ${ANDROID_LIB_ROOT}/armeabi
-    mkdir -p ${ANDROID_LIB_ROOT}/armeabi-v7a
-    mkdir -p ${ANDROID_LIB_ROOT}/x86
-
     git clean -dfx && git checkout -f
     ./Configure dist
-
-    ANDROID_PLATFORM_VERSION=android-19
-    ANDROID_TOOLCHAIN_DIR=/tmp/sqlcipher-android-toolchain
-    OPENSSL_CONFIGURE_OPTIONS="-no-krb5 no-idea no-camellia
-        no-seed no-bf no-cast no-rc2 no-rc4 no-rc5 no-md2 
-        no-md4 no-ripemd no-rsa no-ecdh no-sock no-ssl2 no-ssl3 
-        no-dsa no-dh no-ec no-ecdsa no-tls1 no-pbe no-pkcs
-        no-tlsext no-pem no-rfc3779 no-whirlpool no-ui no-srp
-        no-ssltrace no-tlsext no-mdc2 no-ecdh no-engine
-        no-tls2 no-srtp -fPIC"
-
-    # arm build
-    ${ANDROID_NDK_ROOT}/build/tools/make-standalone-toolchain.sh \
-        --platform=${ANDROID_PLATFORM_VERSION} \
-        --install-dir=${ANDROID_TOOLCHAIN_DIR} \
-        --arch=arm
-
-    export PATH=${ANDROID_TOOLCHAIN_DIR}/bin:$PATH
-
-    RANLIB=arm-linux-androideabi-ranlib \
-        AR=arm-linux-androideabi-ar \
-        CC=arm-linux-androideabi-gcc \
-        ./Configure android ${OPENSSL_CONFIGURE_OPTIONS}
-
-    make clean
-    make build_crypto
-    mv libcrypto.a ${ANDROID_LIB_ROOT}/armeabi/
     
-    rm -rf ${ANDROID_TOOLCHAIN_DIR}
+    for SQLCIPHER_TARGET_PLATFORM in armeabi armeabi-v7a x86 x86_64 arm64-v8a
+    do
+        echo "Building for libcrypto.a for ${SQLCIPHER_TARGET_PLATFORM}"
+        case "${SQLCIPHER_TARGET_PLATFORM}" in
+            armeabi)
+                TOOLCHAIN_ARCH=arm
+                TOOLCHAIN_PREFIX=arm-linux-androideabi
+                CONFIGURE_ARCH=android
+                PLATFORM_OUTPUT_DIR=armeabi
+                ;;
+            armeabi-v7a)
+                TOOLCHAIN_ARCH=arm
+                TOOLCHAIN_PREFIX=arm-linux-androideabi
+                CONFIGURE_ARCH=android -march=armv7-a
+                PLATFORM_OUTPUT_DIR=armeabi-v7a
+                ;;
+            x86)
+                TOOLCHAIN_ARCH=x86
+                TOOLCHAIN_PREFIX=i686-linux-android
+                CONFIGURE_ARCH=android-x86
+                PLATFORM_OUTPUT_DIR=x86
+                ;;
+            x86_64)
+                TOOLCHAIN_ARCH=x86_64
+                TOOLCHAIN_PREFIX=x86_64-linux-android
+                CONFIGURE_ARCH=android64
+                PLATFORM_OUTPUT_DIR=x86_64
+                ;;
+            arm64-v8a)
+                TOOLCHAIN_ARCH=arm64
+                TOOLCHAIN_PREFIX=aarch64-linux-android
+                CONFIGURE_ARCH=android64-aarch64
+                PLATFORM_OUTPUT_DIR=arm64-v8a
+                ;;                
+            *)
+                echo "Unsupported build platform:${SQLCIPHER_TARGET_PLATFORM}"
+                exit 1
+        esac
 
-    #armv7 build
-    ${ANDROID_NDK_ROOT}/build/tools/make-standalone-toolchain.sh \
-        --platform=${ANDROID_PLATFORM_VERSION} \
-        --install-dir=${ANDROID_TOOLCHAIN_DIR} \
-        --arch=arm
+        mkdir -p "${ANDROID_LIB_ROOT}/${SQLCIPHER_TARGET_PLATFORM}"
+        ${ANDROID_NDK_ROOT}/build/tools/make-standalone-toolchain.sh \
+                           --platform=${ANDROID_PLATFORM_VERSION} \
+                           --install-dir=${ANDROID_TOOLCHAIN_DIR} \
+                           --arch=${TOOLCHAIN_ARCH}
+        
+        export PATH=${ANDROID_TOOLCHAIN_DIR}/bin:$PATH
+        export CROSS_SYSROOT=${ANDROID_TOOLCHAIN_DIR}/sysroot
+    
+        RANLIB=${TOOLCHAIN_PREFIX}-ranlib \
+              AR=${TOOLCHAIN_PREFIX}-ar \
+              CC=${TOOLCHAIN_PREFIX}-gcc \
+              ./Configure "${CONFIGURE_ARCH}" "${OPENSSL_CONFIGURE_OPTIONS}"
 
-    export PATH=${ANDROID_TOOLCHAIN_DIR}/bin:$PATH
+        if [ $? -ne 0 ]; then
+            echo "Error executing:./Configure ${CONFIGURE_ARCH} ${OPENSSL_CONFIGURE_OPTIONS}"
+            exit 1
+        fi
+        
+        make clean
+        make
 
-    RANLIB=arm-linux-androideabi-ranlib \
-        AR=arm-linux-androideabi-ar \
-        CC=arm-linux-androideabi-gcc \
-        ./Configure android-armv7 ${OPENSSL_CONFIGURE_OPTIONS}
-
-    make clean
-    make build_crypto
-    mv libcrypto.a ${ANDROID_LIB_ROOT}/armeabi-v7a/
-
-    rm -rf ${ANDROID_TOOLCHAIN_DIR}    
-
-    # x86 build
-    ${ANDROID_NDK_ROOT}/build/tools/make-standalone-toolchain.sh \
-        --platform=${ANDROID_PLATFORM_VERSION} \
-        --install-dir=${ANDROID_TOOLCHAIN_DIR} \
-        --arch=x86
-
-    export PATH=${ANDROID_TOOLCHAIN_DIR}/bin:$PATH
-
-    RANLIB=i686-linux-android-ranlib \
-        AR=i686-linux-android-ar \
-        CC=i686-linux-android-gcc \
-        ./Configure android-x86 ${OPENSSL_CONFIGURE_OPTIONS}
-
-    make clean
-    make build_crypto
-    mv libcrypto.a ${ANDROID_LIB_ROOT}/x86/
+        if [ $? -ne 0 ]; then
+            echo "Error executing make for platform:${SQLCIPHER_TARGET_PLATFORM}"
+            exit 1
+        fi
+        
+        mv libcrypto.a ${ANDROID_LIB_ROOT}/${PLATFORM_OUTPUT_DIR}
+        rm -rf ${ANDROID_TOOLCHAIN_DIR}
+    done    
 )
