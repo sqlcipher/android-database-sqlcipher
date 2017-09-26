70162729f26e9801b39b83851ebe911f7fa18096
Nick Parker <nparker@zetetic.net>
2017-09-26 14:55:02 -0500

    Remove files to prepare for Gradle structure

--- jni/jni_elements.h
+++
@@ -1,3 +0,0 @@
-#ifndef NELEM
-# define NELEM(x) ((int) (sizeof(x) / sizeof((x)[0])))
-#endif
--- src/net/sqlcipher/database/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Contains the SQLCipher database managements classes that an application would use to manage its own private database.
- */
-package net.sqlcipher.database;
